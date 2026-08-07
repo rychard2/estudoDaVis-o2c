4a3632af9efc53cdb7a4a13e508e55d35f235abd
rychard2 <rodrigues.rychard@escola.pr.gov.br>
2026-08-07 09:04:48 -0300

    ok

---
+++ script.js
@@ -0,0 +1,26 @@
+//Botões de acessibilidade
+document.addEventListener("DOMContentLoaded,()=>{
+    
+ const btnContraste = document.getElementById("btn-contraste");  
+ const btnAumentar = docunent.getElementById("btn-aumentar-texto");
+ const btnDiminuir = document.getElementById("btn-diminuir-texto");
+
+ //aumenta a fonte
+
+ let tamanhoAtualFonte = 100;
+
+ //função alto contraste
+
+ btnContraste.addEventListener("click , () =>{"
+
+docunent.bdy.classList.toogle("alto-contraste");
+
+//acessibilidade para leitores de tela 
+
+const ativo = document.body.classList.contains("alto-contraste");
+btnContraste.setAttrbute("aria-pressed , ativo");
+    
+ )
+
+
+}
