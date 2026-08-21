df84e5c33e5f9ccb1f1ad7c27ca3dbb0a139968c
rychard2 <rodrigues.rychard@escola.pr.gov.br>
2026-08-21 11:39:35 -0300

    OK

--- script.js
+++ script.js
@@ -1,42 +1,161 @@
 //Botões de acessibilidade
+
 document.addEventListener("DOMContentLoaded,()=>{
-    
- const btnContraste = document.getElementById("btn-contraste");  
- const btnAumentar = docunent.getElementById("btn-aumentar-texto");
- const btnDiminuir = document.getElementById("btn-diminuir-texto");
 
- //aumenta a fonte
 
- let tamanhoAtualFonte = 100;
+const btnContraste = document.getElementById("btn-contraste");
 
- //função alto contraste
+const btnAumentar = docunent.getElementById("btn-aumentar-texto");
 
- btnContraste.addEventListener("click , () =>{"
+const btnDiminuir = document.getElementById("btn-diminuir-texto");
+
+
+
+//aumenta a fonte
+
+
+
+let tamanhoAtualFonte = 100;
+
+
+
+//função alto contraste
+
+
+
+btnContraste.addEventListener("click , () =>{"
+
+
 
 docunent.bdy.classList.toogle("alto-contraste");
 
-//acessibilidade para leitores de tela 
+
+
+//acessibilidade para leitores de tela
+
+
 
 const ativo = document.body.classList.contains("alto-contraste");
+
 btnContraste.setAttrbute("aria-pressed , ativo");
-    
- )};
+
+
+)};
+
+
 
 //função aumentar o texto
 
+
+
 btnAumentar.addEventListener("click", ()=>{
-    if (tamanhoAtualFonte < 150){
-        tamanhoAtualFonte += 10;
-        document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
-      }
+
+if (tamanhoAtualFonte < 150){
+
+tamanhoAtualFonte += 10;
+
+document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+
+}
+
 });
+
+
 
 //função diminuir texto;
 
+
+
 btnDiminuir.addEventListener("click", ()=> {
-    if (tamanhoAtualFonte > 90){
-        tamanhoAtualFonte -= 10;
-        document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
-    }
+
+if (tamanhoAtualFonte > 90){
+
+tamanhoAtualFonte -= 10;
+
+document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+
+}
+
 });
+
+});//Botões de acessibilidade
+
+document.addEventListener("DOMContentLoaded,()=>{
+
+
+const btnContraste = document.getElementById("btn-contraste");
+
+const btnAumentar = docunent.getElementById("btn-aumentar-texto");
+
+const btnDiminuir = document.getElementById("btn-diminuir-texto");
+
+
+
+//aumenta a fonte
+
+
+
+let tamanhoAtualFonte = 100;
+
+
+
+//função alto contraste
+
+
+
+btnContraste.addEventListener("click , () =>{"
+
+
+
+docunent.bdy.classList.toogle("alto-contraste");
+
+
+
+//acessibilidade para leitores de tela
+
+
+
+const ativo = document.body.classList.contains("alto-contraste");
+
+btnContraste.setAttrbute("aria-pressed , ativo");
+
+
+)};
+
+
+
+//função aumentar o texto
+
+
+
+btnAumentar.addEventListener("click", ()=>{
+
+if (tamanhoAtualFonte < 150){
+
+tamanhoAtualFonte += 10;
+
+document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+
+}
+
 });
+
+
+
+//função diminuir texto;
+
+
+
+btnDiminuir.addEventListener("click", ()=> {
+
+if (tamanhoAtualFonte > 90){
+
+tamanhoAtualFonte -= 10;
+
+document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+
+}
+
+});
+
+}); 
